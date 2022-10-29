85174787311aeb6399e74c307f6c450822929cdf
argyrides <alexandros.argyrides@gmail.com>
2022-10-30 01:54:47 +0300

    Commit

---
+++ FIFO/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static int[] queue = new int[5];
+        static int pivot = -1;
+
+        static void Main(string[] args)
+        {
+            EnQueue(10);
+            EnQueue(25);
+            EnQueue(31);
+            EnQueue(42);
+
+            int removedNumber = DeQueue();
+            Console.WriteLine("Removed:" + removedNumber);
+
+            EnQueue(100);
+            EnQueue(50);
+            EnQueue(37);
+
+            removedNumber = DeQueue();
+            removedNumber = DeQueue();
+
+
+            PrintQueue();
+        }
+
+
+        public static void EnQueue(int num)
+        {
+            if (!IsFull())
+            {
+                pivot++;
+                queue[pivot] = num;
+            }
+
+        }
+
+        public static int DeQueue()
+        {
+            if (!IsEmpty())
+            {
+                pivot--;
+                int removedNumber = queue[0];
+                for (int i = 0; i <= pivot; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
+                queue[pivot + 1] = 0;
+
+                return removedNumber;
+            }
+            return -1;
+        }
+
+        public static bool IsEmpty()
+        {
+            return (pivot == -1) ? true : false;
+        }
+
+        public static bool IsFull()
+        {
+            return (pivot == queue.Length - 1) ? true : false;
+        }
+
+        public static void PrintQueue()
+        {
+            for (int i = 0; i <= pivot; i++)
+            {
+                Console.WriteLine(queue[i]);
+            }
+        }
+
+
+    }
+}
---
+++ LIFO/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static int[] stack = new int[50];
+        static int pivot = -1;
+        static void Main(string[] args)
+        {
+            int remove = Pop();
+
+            Push(5);
+            Push(6);
+            Push(10);
+            Push(3);
+
+            remove = Pop();
+
+            Console.WriteLine("First Pop:");
+            PrintStack();
+
+            Console.WriteLine("Second Pop:");
+            remove = Pop();
+            PrintStack();
+
+
+            Console.WriteLine("Third Pop:");
+
+            Push(30);
+            Push(100);
+            Push(200);
+
+            remove = Pop();
+            Push(150);
+            Push(1343);
+            PrintStack();
+
+        
+
+
+        }
+
+        public static void Push(int num)
+        {
+            if (!IsFull())
+            {
+                pivot++;
+                stack[pivot] = num;
+            }
+            
+        }
+
+        public static int Pop()
+        {
+            int removedNumber;
+            if (!IsEmpty())
+            {
+                removedNumber = stack[pivot];
+                stack[pivot] = 0;
+                pivot--;
+
+                return removedNumber;
+            }
+            return -1;
+        }
+
+        public static bool IsEmpty()
+        {
+            return (pivot == -1) ? true : false;
+        }
+
+        public static bool IsFull() 
+        {
+            return (pivot == stack.Length - 1) ? true : false;
+        }
+
+        public static void PrintStack()
+        {
+            for (int i=0; i <= pivot; i++)
+            {
+                Console.WriteLine(stack[i]);
+            }
+        }
+
+     
+    }
+}
